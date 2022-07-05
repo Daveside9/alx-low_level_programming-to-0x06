5ca26d7194bb6802554882c3121c48a45f903710
Daveside9 <daveside00468@gmail.com>
2022-07-05 16:48:07 -0700

    Test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
-* main - prints putchar
-* Return: Always 0
-*/
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	_putchar ('_');
-	_putchar ('p');
-	_putchar ('u');
-	_putchar ('t');
-	_putchar ('c');
-	_putchar ('h');
-	_putchar ('a');
-	_putchar ('r');
-	_putchar ('\n');
-
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
