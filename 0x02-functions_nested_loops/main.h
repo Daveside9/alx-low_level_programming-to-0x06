ea37a4bcf2dd096cd16afd6acb965c1ac95b2e5a
Daveside9 <daveside00468@gmail.com>
2022-07-05 17:46:33 -0700

    Test

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-#include "main.h" 
+#include "main.h"
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 
- */
+* main - prints _putchar, followed by a new line
+* Return: Always 0
+*/
 
 int main(void)
 {
@@ -16,6 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
